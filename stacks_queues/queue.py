5ac40d85302ffd4291f632c8a404977ad2e1eb66
Sneha Agarwal <68921168+sneha8412@users.noreply.github.com>
2021-09-26 15:24:25 -0700

    Queue tests pass

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -23,7 +23,22 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        if (self.rear + 1) % self.buffer_size == self.front:
+            #The Queue is full
+            raise QueueFullException("Queue is full")
+        elif self.front == -1:
+            # Queue is empty
+            self.front = 0
+            self.rear = 0
+            self.store[self.rear] = element
+            self.size += 1
+        else:
+            self.rear = (self.rear +1) % self.buffer_size
+            # rear needs to wrap around
+            self.store[self.rear] = element
+            self.size += 1
+        
+    
 
     def dequeue(self):
         """ Removes an element from the Queue
@@ -31,27 +46,50 @@ class Queue:
             The Queue is empty.
             returns None
         """
-        pass
+        if self.front == -1:
+            #queue is empty
+            raise QueueEmptyException("Queue is empty")
+        
+        
+        data = self.store[self.front]
+        #override the element being deleted
+        self.store[self.front] = None
+        self.size -= 1
+        
+        #if the queue is empty
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
+        elif self.front == self.buffer_size -1:
+            
+            self.front = 0
+        else:
+            self.front += 1
+        
+        return data
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        return self.store[self.front]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        if self.size == 0:
+            return True
+    
+        return False
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -59,4 +97,16 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        data = []
+        
+        if self.front == -1:
+            return str(data)
+        elif self.front <= self.rear:
+            for i in range(self.front, (self.rear+1)):
+                data.append(self.store[i])
+        else:
+            for i in range(self.front, self.buffer_size):
+                data.append(self.store[i])
+            for i in range(0, self.rear +1):
+                data.append(self.store[i])
+        return str(data)
