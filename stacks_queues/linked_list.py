1c7082b9acf39bca97bae3b89d2a4bf0f0c9b661
Sidney DuPont <sidneyadupont@gmail.com>
2021-09-29 20:23:05 -0700

    Passes all tests except test_with_large_queue

--- stacks_queues/linked_list.py
+++ stacks_queues/linked_list.py
@@ -43,12 +43,12 @@ class LinkedList:
         self.head = self.head.next
         if self.head:
             self.head.previous = None
-        
+
         return value
 
 
     def empty(self):
-      return not self.head
+        return not self.head
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
@@ -57,7 +57,7 @@ class LinkedList:
     def search(self, value):
         if not self.head:
             return False
-        
+
         if self.head.value == value:
             return True
 
@@ -66,7 +66,7 @@ class LinkedList:
             if current.value == value:
                 return True
             current = current.next
-        
+
         return False
 
 
@@ -77,7 +77,7 @@ class LinkedList:
     def find_max(self):
         if not self.head:
             return None
-        
+
         current = self.head
         max = current.value
 
@@ -102,7 +102,7 @@ class LinkedList:
             if min > current.value:
                 min = current.value
             current = current.next
-        
+
         return min
 
     # method that returns the length of the singly linked list
@@ -111,13 +111,13 @@ class LinkedList:
     def length(self):
         if not self.head:
             return 0
-        
+
         current = self.head
         length = 0
         while current:
             length += 1
             current = current.next
-        
+
         return length
 
     # method that returns the value at a given index in the linked list
@@ -136,7 +136,7 @@ class LinkedList:
                 return current.value
             count += 1
             current = current.next
-        
+
         return None
 
     # method to print all the values in the linked list
@@ -149,7 +149,7 @@ class LinkedList:
         while current:
             values.append(current.value)
             current = current.next
-        
+
         print(", ".join(values))
 
     # method to delete the first node found with specified value
@@ -158,7 +158,7 @@ class LinkedList:
     def delete(self, value):
         if not self.head:
             return None
-        
+
         current = self.head
         if current.value == value:
             self.head = current.next
@@ -210,7 +210,7 @@ class LinkedList:
         if not self.head:
             self.add_first(value)
             return
-    
+
         self.tail.next = new_node
         new_node.previous = self.tail
         self.tail = new_node
--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -15,7 +15,6 @@ class Queue:
         self.front = -1
         self.rear = -1
         self.size = 0
-      
 
     def enqueue(self, element):
         """ Adds an element to the Queue
@@ -23,35 +22,50 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        if self.size >= self.buffer_size:
+            raise QueueFullException()
+
+        self.rear = (self.rear + 1) % self.buffer_size
+        self.store[self.rear] = element
+        self.size += 1
 
     def dequeue(self):
         """ Removes an element from the Queue
-            Raises a QueueEmptyException if 
+            Raises a QueueEmptyException if
             The Queue is empty.
             returns None
         """
-        pass
+        if self.empty():
+            raise QueueEmptyException()
+
+        self.front = (self.front + 1) % self.buffer_size
+        temp_front = self.store[self.front]
+        self.store[self.front] = None
+        self.size -= 1
+        return temp_front
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
-        
+        if self.empty():
+            return None
+        return self.store[self.front]
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        if self.size == 0:
+            return True
+        return False
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -59,4 +73,34 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        queue_list = []
+
+        if self.empty():
+            return str(queue_list)
+
+        for i in range(self.front + 1, self.buffer_size):
+            if self.store[i] != None:
+                queue_list.append(self.store[i])
+
+        if self.rear < self.front:
+            # why isn't this if statement being hit?
+            # does it have to do with the way I'm setting rear in enqueue()?
+            for i in range(0, self.rear + 1):
+                if self.store[i] != None:
+                    queue_list.append(self.store[i])
+
+        return str(queue_list)
+
+        # if self.front <= self.rear:
+        #     for elem in self.store:
+        #         if elem != None:
+        #             queue_list.append(elem)
+        #     return str(queue_list)
+        # else:
+        #     # test is never hitting this else statement...why?
+        #     for i in range(0, self.rear + 1):
+        #         queue_list.append(self.store[i])
+
+        #     for i in range(self.front, self.rear):
+        #         queue_list.append(self.store[i])
+        #     return str(queue_list)
