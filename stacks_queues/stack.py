79d71bd56929a88cb7d1719949fa9e79ce79ab65
Nandita Gilroy <c-ngilroy@zulily.com>
2021-10-11 13:35:50 -0700

    adds logic for stack methods

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -12,7 +12,7 @@ class Stack:
         """ Adds an element to the top of the Stack.
             Returns None
         """
-        pass
+        self.store.add_first(element)
 
     def pop(self):
         """ Removes an element from the top
@@ -21,13 +21,16 @@ class Stack:
             The Stack is empty.
             returns None
         """
-        pass
+
+        if self.empty():
+            raise StackEmptyException()
+        return self.store.remove_first()
 
     def empty(self):
         """ Returns True if the Stack is empty
             And False otherwise
         """
-        pass
+        return self.store.length() == 0
 
     def __str__(self):
         """ Returns the Stack in String form like:
@@ -35,4 +38,9 @@ class Stack:
             Starting with the top of the Stack and
             ending with the bottom of the Stack.
         """
-        pass
+        current = self.pop()
+        stack_list = []
+        while current:
+            stack_list.append(current)
+            current = self.pop()
+        return str(stack_list)
