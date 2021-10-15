75e1b76193d8285ac0dc3607c29313789962add2
weishanyang <weishanyang33@gmail.com>
2021-10-14 22:34:21 -0700

    pass queue test.

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -23,7 +23,13 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        if self.size == self.buffer_size:
+            raise QueueFullException()
+        if self.size == 0:
+            self.front = 0
+        self.rear = (self.rear + 1) % self.buffer_size
+        self.size += 1
+        self.store[self.rear] = element
 
     def dequeue(self):
         """ Removes an element from the Queue
@@ -31,27 +37,38 @@ class Queue:
             The Queue is empty.
             returns None
         """
-        pass
+        if self.size == 0:
+            raise QueueEmptyException()
+        # front_value = self.store[self.front]
 
-    def front(self):
+        front_value = self.front_value() 
+        self.front = (self.front + 1) % self.buffer_size
+        self.size -= 1
+        return front_value
+    #Want to use self.front() to get the front value, but self.front is already an attribute name
+    #change it to front_value
+    def front_value(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        if self.empty():
+            return None
+        return self.store[self.front]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        return self.size == 0
+            
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -59,4 +76,8 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        values = []
+        for i in range(self.front, self.front + self.size):
+            i = i % self.buffer_size
+            values.append(self.store[i])
+        return str(values)
