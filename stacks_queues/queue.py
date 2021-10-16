465bf39ea4a1451c24848d832c39f0dafb056adc
Christian Spencer <64983017+Crintion@users.noreply.github.com>
2021-10-16 09:29:44 -0700

    All tests passing

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -23,7 +23,17 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        # if self.buffer_size > 20:
+        #     raise QueueFullException()
+        is_added = False
+        for i in range(len(self.store)):
+            if self.store[i] is None:
+                self.store[i] = element
+                is_added = True
+                break
+        if not is_added:
+            raise QueueFullException
+        
 
     def dequeue(self):
         """ Removes an element from the Queue
@@ -31,27 +41,47 @@ class Queue:
             The Queue is empty.
             returns None
         """
-        pass
+        if self.empty():
+            raise QueueEmptyException
+        val = self.store[0]
+        self.store.pop(0)
+        self.store.append(None)
+        return val
+        
+        
+
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        return self.store[0]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        count = 0
+        for element in self.store:
+            if element is not None:
+                count += 1
+        return count
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        count = 0
+        for element in self.store:
+            if element is not None:
+                count += 1
+        
+        if count == 0:
+            return True
+        else:
+            return False
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -59,4 +89,12 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        l = []
+        for i in self.store:
+            if i is not None:
+                l.append(i)
+        return str(l)
+
+
+
+        
