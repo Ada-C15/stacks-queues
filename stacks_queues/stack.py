8ff3e8311e1a8ffd94513e934ea036c08ff3fcc6
Kristin <Kristineliseboyd13@gmail.com>
2021-11-11 13:12:36 -0800

    passes all tests

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -1,7 +1,9 @@
 from stacks_queues.linked_list import LinkedList
 
+
 class StackEmptyException(Exception):
     pass
+
 
 class Stack:
 
@@ -12,7 +14,8 @@ class Stack:
         """ Adds an element to the top of the Stack.
             Returns None
         """
-        pass
+        self.store.add_last(element)
+        return None
 
     def pop(self):
         """ Removes an element from the top
@@ -21,13 +24,18 @@ class Stack:
             The Stack is empty.
             returns None
         """
-        pass
+        # if stack is empty -> raise StackEmptyException (is there anything in this class??)
+        if self.store.length() == 0:
+            raise StackEmptyException
+        else:
+            return self.store.remove_last()
 
     def empty(self):
         """ Returns True if the Stack is empty
             And False otherwise
         """
-        pass
+        if self.store.length() == 0:
+            return True
 
     def __str__(self):
         """ Returns the Stack in String form like:
@@ -35,4 +43,6 @@ class Stack:
             Starting with the top of the Stack and
             ending with the bottom of the Stack.
         """
-        pass
+        # starting w/ top of stack -> bottom of stack --> need to reverse
+        self.store.reverse()
+        return self.store.__str__()
