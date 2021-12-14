055f83e189edf3200de38caed5e3bbf6f18ac7cc
Drew Taylor <drewlynntaylor@gmail.com>
2021-12-14 00:16:31 -0800

    Passing all tests

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -15,7 +15,6 @@ class Queue:
         self.front = -1
         self.rear = -1
         self.size = 0
-      
 
     def enqueue(self, element):
         """ Adds an element to the Queue
@@ -23,34 +22,51 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        if self.buffer_size == self.size:
+            raise QueueFullException()
+        
+        self.store[self.rear] = element
+        self.rear = (self.rear + 1) % self.buffer_size
+        self.size += 1 
 
     def dequeue(self):
         """ Removes and returns an element from the Queue
             Raises a QueueEmptyException if 
             The Queue is empty.
         """
-        pass
+        if self.size == 0: 
+            raise QueueEmptyException()
+
+        element = self.store[self.front]
+        self.front = (self.front + 1) % self.buffer_size
+
+        if self.front == self.rear: 
+            self.front = 0 
+            self.rear = 0 
+        
+        self.size -= 1
+        return element
+
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        return self.store[self.front]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        return self.size == 0
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -58,4 +74,8 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        queue_list = []
+        for ele in range(self.front, self.front + self.size): 
+            ele = ele % self.buffer_size 
+            queue_list.append(self.store[ele])
+        return str(queue_list)
